01508ed2e143e67b892b6ebc63bc439a398ca87a
Mithun Manivannan <mithunm93@gmail.com>
2016-03-12 12:20:31 -0800

    removed private.js

--- private.js
+++
@@ -1,5 +0,0 @@
-const private = {
-  slack_url: "https://hooks.slack.com/services/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",
-  firebase_url: "https://pcgaming-claptrap.firebaseio.com/sale_check"
-};
-module.exports = private
